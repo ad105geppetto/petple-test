b989acf6f2b7017d70b7a4ad431280d96d525c00
ad105geppetto <ad105geppetto@gmail.com>
2023-02-20 03:42:36 +0900

    init: 프로젝트 초기설정

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { type AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: "https://backendonline.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
