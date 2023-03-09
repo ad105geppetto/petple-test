b7098d40f241ff20838f433346f67614df49a4dd
ad105geppetto <ad105geppetto@gmail.com>
2023-03-09 23:15:50 +0900

    test: 자유게시판 등록 페이지 Unit Test 설정 변경
     - 게시물 등록시 react-quill을 사용하기에 설정을 변경합니다.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
+  moduleNameMapper: {
+    // ReactQuill을 모의(mock) 모듈로 대체합니다.
+    "react-quill": "<rootDir>/__mocks__/react-quill.tsx",
+  },
 });
 
 // Add any custom config to be passed to Jest
