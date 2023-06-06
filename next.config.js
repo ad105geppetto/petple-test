29f86f9ee61105c094f4a492eb64fc4b5a42b939
ad105geppetto <ad105geppetto@gmail.com>
2023-06-06 22:25:01 +0900

    update: next.config.js 변경

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   trailingSlash: true,
   exclude: ["__test__/**/*", "__mocks__/**/*"],
   generateBuildId: () => "petple_deploy",
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination: "https://localhost:3000",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
